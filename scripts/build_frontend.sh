b104e9fb03adf6946e7f4968b1cda757e98bbfb5
Aryan Chopra <135122815+SpartanPike@users.noreply.github.com>
2024-10-04 23:47:40 +0530

    Update build_frontend.sh

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /var/www/vendingapp
-# Build the frontend React app
+
+# Navigate to the project directory
+cd /home/ec2-user/VendingWebsite
+
+# Install dependencies
+npm install
+
+# Build the frontend
 npm run build
+
+# Output a success message
+echo "Frontend build completed successfully."
