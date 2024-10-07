67777b83a95db1a57d3fb2e41703d14779615eec
Aryan Chopra <135122815+SpartanPike@users.noreply.github.com>
2024-10-07 21:15:27 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the project directory
-cd /home/ubuntu/VendingWebsite
+cd /home/ec2-user/VendingWebsite
 
 # Start the Node.js server (assuming your backend server is defined in your project)
 npm start
