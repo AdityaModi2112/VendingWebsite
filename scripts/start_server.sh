a2395375ef598b019342480f02aeda72205ae674
Aryan Chopra <135122815+SpartanPike@users.noreply.github.com>
2024-10-07 22:38:17 +0530

    Delete scripts directory

--- scripts/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the project directory
-cd /home/ec2-user/VendingWebsite
-
-# Start the Node.js server (assuming your backend server is defined in your project)
-npm start
